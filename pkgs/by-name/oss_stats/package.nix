194a3a98ee5199cdf95e626169f90f90dbb3d0c8
Josh Holman <josh@jolman.me>
2025-07-23 13:25:35 -0700

    init packages

--- pkgs/by-name/oss_stats/package.nix
+++ pkgs/by-name/oss_stats/package.nix
@@ -1,2 +1,33 @@
-{}: {
+{
+  buildPythonPackage,
+  fetchFromGitHub,
+  hatchling,
+  lib,
+}:
+buildPythonPackage rec {
+  pname = "oss_stats";
+  version = "0.1.0";
+  pyproject = true;
+  src = fetchFromGitHub {
+    owner = "acmcsufoss";
+    repo = "oss_stats";
+    rev = "v${version}";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  build-system = [
+    hatchling
+  ];
+
+  dependencies = [
+  ];
+
+  meta = with lib; {
+    description = "";
+    homepage = "https://github.com/acmcsufoss/oss_stats";
+    license = licenses.mit;
+    maintainers = with maintainers; [TheJolman];
+    mainProgram = "oss_stats";
+  };
 }
+
--- pkgs/top-level.nix
+++ pkgs/top-level.nix
@@ -1,2 +1,7 @@
-{}: {
+{
+  pkgs,
+  api-acmcsuf,
+}: {
+  oss_stats = pkgs.callPackages ./by-name/oss_stats/package.nix {};
+  inherit (api-acmcsuf.packages.${pkgs.system}.default) api-acmcsuf;
 }
